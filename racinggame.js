c7a96892df5d647a50efc51ba22860e643118428
Yonas Berhe <yonas.berhe007@gmail.com>
2014-09-14 00:46:48 -0700

    Update racinggame.js

--- racinggame.js
+++ racinggame.js
@@ -1,24 +1,25 @@
 <script>
-var Usian (start, name, speed, ) {
+
+var Usian  {}
+
   Usian.start = 1.1;
-  Usian.name = Usian;
+  Usian.name = 'Usian'';
   Usian.speed = 9.53;
-  }
-  function (){
+  Usian.checkWinner = function (){
     return Math.random() * (1.4 - 1.1)
   };
 
-
-  var Yohan(start, name, speed) {
+  var Yohan {}
+  
     Yohan.start = 1.23;
     Yohan.name = Yohan Blake;
     Yohan.speed = 9.23;
-  };
-  function() {
+    function() {
+    Yohan.checkWinner = function(){ 
     return Math.random() * (1.4 - 1.1)
   };
-
-
+  
+  
 
 
 
