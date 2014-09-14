8b3252f171bb51681d781eb9120c703c17bb49da
Yonas Berhe <yonas.berhe007@gmail.com>
2014-09-13 19:10:14 -0700

    Create racinggame.js
    Usian Bolt Vs Yohan Blake
    
    this code will specifically mimic the 100 meter dash in  2012 in Jamaica. Yohan Blake beat Usian Bolt

---
+++ racinggame.js
@@ -0,0 +1,25 @@
+<script>
+var Usian (start, name, speed, ) {
+  Usian.start = 1.1;
+  Usian.name = Usian;
+  Usian.speed = 9.53;
+  }
+  function (){
+    return Math.random() * (1.4 - 1.1)
+  };
+
+
+  var Yohan(start, name, speed) {
+    Yohan.start = 1.23;
+    Yohan.name = Yohan Blake;
+    Yohan.speed = 9.23;
+  };
+  function() {
+    return Math.random() * (1.4 - 1.1)
+  };
+
+
+
+
+
+</script>
